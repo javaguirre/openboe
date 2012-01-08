a71e7894991ac4222934f3bcee222bd8b5b6f880
Javier Aguirre <contacto@javaguirre.net>
2012-01-08 13:57:58 +0100

    Formatting code to keep pep8 standard

--- app_modules/feed.py
+++ app_modules/feed.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 import feedparser
 
-def parse(url, filter_by = ""):
+
+def parse(url, filter_by=""):
+
     feed = feedparser.parse(url)
     feed_list = []
     #TODO Add in template uls, format date, load image item['summary']
+
     for item in feed['items']:
         if filter_by in item['title'].lower():
             feed_list.append(item)
--- application.py
+++ application.py
@@ -3,38 +3,39 @@ from flask import Flask, render_template, request, abort, redirect, url_for
 from app_modules.feed import parse
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    return "Hello World!"
-    
+    return render_template("index.html")
+
+
 @app.route("/feed/<name>", methods=['GET', 'POST'])
 def feed(name):
     feeds = ["seccionI", "seccionII", "seccionIII", "seccionIV"]
     topics = {
-              "becas": "http://www.boe.es/rss/canal.php?c=becas",
-              "ayudas": "http://www.boe.es/rss/canal.php?c=ayudas",
-              "estudio": "http://www.boe.es/rss/canal.php?c=planes",
-              "convenios": "http://www.boe.es/rss/canal.php?c=ccolectivos",
-              "divisas": "http://www.boe.es/rss/canal.php?c=cambios",
-              "prestamos": "http://www.boe.es/rss/canal.php?c=prestamos",
-              "servicios": "http://www.boe.es/rss/canal.php?c=cartas", 
-              "colaboracion": "http://www.boe.es/rss/canal.php?c=ccolaboracion",
-              "premios": "http://www.boe.es/rss/canal.php?c=premios"
+              "becas": "/rss/canal.php?c=becas",
+              "ayudas": "/rss/canal.php?c=ayudas",
+              "estudio": "/rss/canal.php?c=planes",
+              "convenios": "/rss/canal.php?c=ccolectivos",
+              "divisas": "/rss/canal.php?c=cambios",
+              "prestamos": "/rss/canal.php?c=prestamos",
+              "servicios": "/rss/canal.php?c=cartas",
+              "colaboracion": "/rss/canal.php?c=ccolaboracion",
+              "premios": "/rss/canal.php?c=premios"
               }
     filter_by = ""
-    
+
     if request.method == 'POST':
         #TODO check POST['q']
         filter_by = request.form['q']
 
-    if topics.has_key(name):
-        links = parse(topics[name], filter_by)
+    if name in topics:
+        links = parse("http://www.boe.es" + topics[name], filter_by)
     else:
         abort(404)
-        
+
     return render_template("feeds.html", links=links, query=filter_by)
 
 if __name__ == "__main__":
     app.debug = True
     app.run()
-
