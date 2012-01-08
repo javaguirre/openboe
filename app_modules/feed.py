debbcd3ff94785f4365b8b137ce93bf115976df8
Javier Aguirre <contacto@javaguirre.net>
2012-01-08 20:31:20 +0100

    Added twitter bootstrap for styling. Search filter improved with dates

--- app_modules/feed.py
+++ app_modules/feed.py
@@ -1,15 +1,43 @@
 # -*- coding: utf-8 -*-
 import feedparser
+from datetime import datetime
 
 
-def parse(url, filter_by=""):
+def parse(url, filter_by={}):
 
     feed = feedparser.parse(url)
     feed_list = []
+    filter_text = ""
     #TODO Add in template uls, format date, load image item['summary']
+    try:
+        filter_text = filter_by['q']
+    except KeyError as msg:
+        print 'Doesnt exist: %s' % msg
+
+    print filter_text
+    try:
+        filter_from = filter_by['from_date']
+        filter_from = datetime.strptime(filter_from, "%m/%d/%Y")
+    except (ValueError, KeyError) as msg:
+        print 'Doesnt exist from_date: %s' % msg
+        filter_from = ""
+
+    try:
+        filter_to = filter_by['to_date']
+        filter_to = datetime.strptime(filter_to, "%m/%d/%Y")
+    except (ValueError, KeyError) as msg:
+        print 'Doesnt exist to_date: %s' % msg
+        filter_to = ""
 
     for item in feed['items']:
-        if filter_by in item['title'].lower():
-            feed_list.append(item)
+        if filter_text in item['title'].lower() or filter_text in item['description'].lower():
+            item_date = item['description'].split(" - ")[1].replace("Publicado el", "").strip()
+            item_date = datetime.strptime(item_date, "%d/%m/%Y")
+
+            if filter_from and filter_to:
+                if filter_from <= item_date and filter_to >= item_date:
+                    feed_list.append(item)
+            else:
+                feed_list.append(item)
 
     return feed_list
--- application.py
+++ application.py
@@ -3,38 +3,42 @@ from flask import Flask, render_template, request, abort, redirect, url_for
 from app_modules.feed import parse
 app = Flask(__name__)
 
+topics = {
+          "becas": "/rss/canal.php?c=becas",
+          "ayudas": "/rss/canal.php?c=ayudas",
+          "estudio": "/rss/canal.php?c=planes",
+          "convenios": "/rss/canal.php?c=ccolectivos",
+          "divisas": "/rss/canal.php?c=cambios",
+          "prestamos": "/rss/canal.php?c=prestamos",
+          "servicios": "/rss/canal.php?c=cartas",
+          "colaboracion": "/rss/canal.php?c=ccolaboracion",
+          "premios": "/rss/canal.php?c=premios"
+          }
+
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", topics=topics)
 
 
 @app.route("/feed/<name>", methods=['GET', 'POST'])
 def feed(name):
     feeds = ["seccionI", "seccionII", "seccionIII", "seccionIV"]
-    topics = {
-              "becas": "/rss/canal.php?c=becas",
-              "ayudas": "/rss/canal.php?c=ayudas",
-              "estudio": "/rss/canal.php?c=planes",
-              "convenios": "/rss/canal.php?c=ccolectivos",
-              "divisas": "/rss/canal.php?c=cambios",
-              "prestamos": "/rss/canal.php?c=prestamos",
-              "servicios": "/rss/canal.php?c=cartas",
-              "colaboracion": "/rss/canal.php?c=ccolaboracion",
-              "premios": "/rss/canal.php?c=premios"
-              }
-    filter_by = ""
+
+    filter_by = {}
 
     if request.method == 'POST':
         #TODO check POST['q']
-        filter_by = request.form['q']
+        filter_by = {"q": request.form['q'],
+                     "from_date": request.form['from'], "to_date": request.form['to']}
 
     if name in topics:
         links = parse("http://www.boe.es" + topics[name], filter_by)
     else:
         abort(404)
 
-    return render_template("feeds.html", links=links, query=filter_by)
+    return render_template("feeds.html",
+                           links=links, query=filter_by, topics=topics)
 
 if __name__ == "__main__":
     app.debug = True
