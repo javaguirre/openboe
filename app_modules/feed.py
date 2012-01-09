637560d65e5a95fe1a63145497e97a5fc87eda21
Javier Aguirre <contacto@javaguirre.net>
2012-01-09 20:53:22 +0100

    Adding boe and borme

--- app_modules/feed.py
+++ app_modules/feed.py
@@ -10,46 +10,63 @@ def replace_all(text, dic):
     return text_formatted
 
 
-def parse(url, filter_by={}):
+def search_text(item_title, item_desc, filter_text):
+    ''' Simple text search '''
+    needles = filter_text.lower().split(" ")
+    found = False
+
+    for needle in needles:
+        if needle in item_title or needle in item_desc:
+            found = True
+        else:
+            found = False
+
+    return found
+
+
+def parse(url, type_feed, filter_by={}):
 
     feed = feedparser.parse(url)
     feed_list = []
-    filter_text = ""
     reps = {u"á": u"a", u"í": u"i", u"ó": u"o", u"é": u"e", u"ú": u"u"}
 
-    #TODO Add in template uls, format date, load image item['summary']
     try:
         filter_text = filter_by['q']
     except KeyError as msg:
-        print 'Doesnt exist: %s' % msg
+        #print 'Doesnt exist: %s' % msg
+        filter_text = ""
 
     print filter_text
     try:
         filter_from = filter_by['from_date']
         filter_from = datetime.strptime(filter_from, "%m/%d/%Y")
     except (ValueError, KeyError) as msg:
-        print 'Doesnt exist from_date: %s' % msg
+        #print 'Doesnt exist from_date: %s' % msg
         filter_from = ""
 
     try:
         filter_to = filter_by['to_date']
         filter_to = datetime.strptime(filter_to, "%m/%d/%Y")
     except (ValueError, KeyError) as msg:
-        print 'Doesnt exist to_date: %s' % msg
+        #print 'Doesnt exist to_date: %s' % msg
         filter_to = ""
 
     for item in feed['items']:
-        item_title = replace_all(item['title'].lower(), reps)
-        item_desc = item['description'].lower()
+        if type_feed == "borme" or type_feed == 'boe':
+            #Provisional
+            feed_list.append(item)
+        else:
+            item_title = replace_all(item['title'].lower(), reps)
+            item_desc = item['description'].lower()
 
-        if filter_text in item_title or filter_text in item_desc:
-            item_date = item['description'].split(" - ")[1].replace("Publicado el", "").strip()
-            item_date = datetime.strptime(item_date, "%d/%m/%Y")
+            if search_text(item_title, item_desc, filter_text):
+                item_date = item['description'].split(" - ")[1].replace("Publicado el", "").strip()
+                item_date = datetime.strptime(item_date, "%d/%m/%Y")
 
-            if filter_from and filter_to:
-                if filter_from <= item_date and filter_to >= item_date:
+                if filter_from and filter_to:
+                    if filter_from <= item_date and filter_to >= item_date:
+                        feed_list.append(item)
+                else:
                     feed_list.append(item)
-            else:
-                feed_list.append(item)
 
     return feed_list
--- application.py
+++ application.py
@@ -3,17 +3,69 @@ from flask import Flask, render_template, request, abort, redirect, url_for
 from app_modules.feed import parse
 app = Flask(__name__)
 
+RSS_URL = "http://www.boe.es/rss/"
+
 topics = {
-          "becas": "/rss/canal.php?c=becas",
-          "ayudas": "/rss/canal.php?c=ayudas",
-          "estudio": "/rss/canal.php?c=planes",
-          "convenios": "/rss/canal.php?c=ccolectivos",
-          "divisas": "/rss/canal.php?c=cambios",
-          "prestamos": "/rss/canal.php?c=prestamos",
-          "servicios": "/rss/canal.php?c=cartas",
-          "colaboracion": "/rss/canal.php?c=ccolaboracion",
-          "premios": "/rss/canal.php?c=premios"
+          "url": "canal.php",
+          "becas": "?c=becas",
+          "ayudas": "?c=ayudas",
+          "estudio": "?c=planes",
+          "convenios": "?c=ccolectivos",
+          "divisas": "?c=cambios",
+          "prestamos": "?c=prestamos",
+          "servicios": "?c=cartas",
+          "colaboracion": "?c=ccolaboracion",
+          "premios": "?c=premios"
           }
+
+boes = {
+          "url": "boe.php",
+          "sumario": "",
+          "seccionI": "?s=1",
+          "seccionII": "?s=2",
+          "seccionIIA": "?s=2A",
+          "seccionIIB": "?s=SB",
+          "seccionIII": "?s=3",
+          "seccionIV": "?s=4",
+          "seccionV": "?s=5",
+          "seccionVA": "?s=5A",
+          "seccionVB": "?s=5B",
+          "seccionVC": "?s=5C",
+          "suplemento": "?s=T"
+        }
+
+bormes = {
+          "url": "borme.php",
+          "sumario": "",
+          "seccionI": "?s=1",
+          "seccionIA": "?s=A",
+          "seccionIB": "?s=B",
+          "seccionII": "?s=C"
+        }
+
+
+#FIXME move it to somewhere
+def treat_request(type_feed, request, name):
+    filter_by = {}
+
+    if type_feed == "boe":
+        rss_topics = boes
+    elif type_feed == 'borme':
+        rss_topics = bormes
+    else:
+        rss_topics = topics
+
+    if request.method == 'POST':
+        #TODO check POST['q']
+        filter_by = {"q": request.form['q'],
+                     "from_date": request.form['from'], "to_date": request.form['to']}
+
+    if name in rss_topics:
+        links = parse(RSS_URL + rss_topics['url'] + rss_topics[name], type_feed, filter_by)
+    else:
+        abort(404)
+
+    return filter_by, links
 
 
 @app.route("/")
@@ -23,22 +75,27 @@ def index():
 
 @app.route("/feed/<name>", methods=['GET', 'POST'])
 def feed(name):
-    feeds = ["seccionI", "seccionII", "seccionIII", "seccionIV"]
-
-    filter_by = {}
-
-    if request.method == 'POST':
-        #TODO check POST['q']
-        filter_by = {"q": request.form['q'],
-                     "from_date": request.form['from'], "to_date": request.form['to']}
-
-    if name in topics:
-        links = parse("http://www.boe.es" + topics[name], filter_by)
-    else:
-        abort(404)
+    filter_by, links = treat_request('topics', request, name)
 
     return render_template("feeds.html",
-                           links=links, query=filter_by, topics=topics)
+                           links=links, query=filter_by, topics=topics, bormes=bormes, boes=boes, name=name)
+
+
+@app.route("/borme/<name>", methods=['GET', 'POST'])
+def borme(name):
+    filter_by, links = treat_request('borme', request, name)
+
+    return render_template("feeds.html",
+                           links=links, query=filter_by, topics=topics, bormes=bormes, boes=boes, name=name)
+
+
+@app.route("/boe/<name>", methods=['GET', 'POST'])
+def boe(name):
+    filter_by, links = treat_request('boe', request, name)
+
+    return render_template("feeds.html",
+                           links=links, query=filter_by, topics=topics, bormes=bormes, boes=boes, name=name)
+
 
 if __name__ == "__main__":
     app.debug = True
