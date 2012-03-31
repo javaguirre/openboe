9098cae461bbba54ea5d9b986557bc5e1935b9b5
Javier Aguirre <contacto@javaguirre.net>
2012-03-31 23:32:51 +0200

    Finishing first script, saving feed urls in db

--- db.py
+++ db.py
@@ -4,13 +4,13 @@ class Db(object):
 
     def __init__(self, db_name='openboe'):
         conn = Connection()
-        self.db = conn['db_name']
+        self.db = conn[db_name]
 
     def get_cols(self, col):
         return self.db[col].find()
 
+    def has(self, col, query_dict):
+        return list(self.db[col].find(query_dict))
+
     def insert(self, col, element):
         self.db[col].insert(element)
-
-    def has(self, col, query_dict):
-        return bool(self.db[col].find(query_dict))
--- get_rss_urls.py
+++ get_rss_urls.py
@@ -16,7 +16,6 @@ feeds = []
 titles = []
 
 for div_class in DIV_FEED_CLASSES:
-    print div_class
     content_tree_elem = content_tree.find_class(div_class)
     div_boxes.extend([content_elem for content_elem in content_tree_elem])
 
@@ -27,14 +26,14 @@ for div_box in div_boxes:
     #print elem.text_content()
     #print div_box.find_tag('h3')
 for feed, title in zip(feeds, titles):
-    print titles
-    print db_obj.has('section', {"title": title.text_content()})
-    if not db_obj.has('section', {"title": title.text_content()}):
+    section_obj = db_obj.has('section', {"title": title.text_content()})
+
+    if not section_obj and title.text_content():
         section_obj = db_obj.insert('section',
-                                    {"title": title.text_content(), "timestamp": time.time()})
-        print section_obj
-    #rss_list.append(link)
-    #print title.text_content()
+                                    {"title": title.text_content(), "timestamp": time()})
+    else:
+        section_obj = section_obj[0]
+
     for elem in feed.iterlinks():
         if not db_obj.has('feed', {"url": elem[2]}):
             db_obj.insert('feed',
