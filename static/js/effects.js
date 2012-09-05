6ae960859acb5f490f34d5b1cc19d4846bb6fa2a
Javier Aguirre <contacto@javaguirre.net>
2012-09-05 19:25:44 +0200

    Added efect for tabs

--- static/js/effects.js
+++ static/js/effects.js
@@ -1,7 +1,11 @@
 $(function () {
     $('.nav-tabs li a').click(function() {
         var href = $(this).attr('href');
+
+        $('.nav-tabs li').removeClass('active');
         $('.tab-pane').removeClass('active');
+
+        $(this).parent().addClass('active');
         $(href).addClass('active');
     });
 });
